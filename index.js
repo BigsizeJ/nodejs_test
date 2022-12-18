cd459dd9b679f06f9fa2e0ad51e95b193ac4dd4e
bigsizeJ <apacjessie@gmail.com>
2022-12-18 21:34:22 +0800

    updating the app to using express

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 8080;
 const { readFile } = require("fs/promises");
 
-const port = 8080;
-
-const head = {
-  "Content-Type": "text/html",
-};
-
-const server = http.createServer(async (req, res) => {
-  if (req.url === "/" || req.url === "/index.html") {
-    const data = await readFile("./index.html", "utf8");
-    res.writeHead(200, head);
-    res.write(data);
-    return res.end();
-  }
-  if (req.url === "/about.html") {
-    const data = await readFile("./about.html", "utf8");
-    res.writeHead(200, head);
-    res.write(data);
-    return res.end();
-  }
-  if (req.url === "/contact-me.html") {
-    const data = await readFile("./contact-me.html", "utf8");
-    res.writeHead(200, head);
-    res.write(data);
-    return res.end();
-  }
-  const data = await readFile("./404.html", "utf8");
-  res.writeHead(200, head);
-  res.write(data);
-  return res.end();
+app.get("/" || "/index.html", async (req, res) => {
+  res.send(await readFile("./index.html", "utf8"));
 });
 
-server.listen(port, (err) => {
-  if (err) return console.log("Error porting");
-  return console.log("Success port at http://localhost:8080");
+app.get("/about.html", async (req, res) => {
+  res.send(await readFile("./about.html", "utf8"));
+});
+
+app.get("/contact-me.html", async (req, res) => {
+  res.send(await readFile("./contact-me.html", "utf8"));
+});
+
+app.use(async (req, res) => {
+  res.status(404).send(await readFile("./404.html", "utf8"));
+});
+
+app.listen(port, (err) => {
+  if (err) Error(err);
+  return console.log(`Success port listening at http://localhost:${port}`);
 });
